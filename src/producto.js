a3c60e5f24b1c41f3aaf129626b53d53a0385eb4
LuciaAnsino <e.luciaansino@go.ugr.es>
2023-10-20 09:07:37 +0200

    [IV-23-24] Objetivo 2
    Modificar tipo de atributos

--- src/producto.js
+++ src/producto.js
@@ -3,39 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Producto = void 0;
 var Producto = /** @class */ (function () {
     function Producto(nombre, carbohidratos, proteinas, grasas) {
-        this._nombre = nombre;
-        this._carbohidratos = carbohidratos;
-        this._proteinas = proteinas;
-        this._grasas = grasas;
+        this.nombre = nombre;
+        this.carbohidratos = carbohidratos;
+        this.proteinas = proteinas;
+        this.grasas = grasas;
     }
-    Object.defineProperty(Producto.prototype, "nombre", {
-        get: function () {
-            return this._nombre;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Producto.prototype, "carbohidratos", {
-        get: function () {
-            return this._carbohidratos;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Producto.prototype, "proteinas", {
-        get: function () {
-            return this._proteinas;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Producto.prototype, "grasas", {
-        get: function () {
-            return this._grasas;
-        },
-        enumerable: false,
-        configurable: true
-    });
+    Producto.prototype.getCarbohidratos = function () {
+        return this.carbohidratos;
+    };
+    Producto.prototype.getProteinas = function () {
+        return this.proteinas;
+    };
+    Producto.prototype.getGrasas = function () {
+        return this.grasas;
+    };
     return Producto;
 }());
 exports.Producto = Producto;
