56dce8e4f437316836073b956eb8ab8308b695d8
LuciaAnsino <e.luciaansino@go.ugr.es>
2023-10-20 09:13:34 +0200

    [IV-23-24] Objetivo 2
    Modificacion de la entidad Compra, creación de un getter indispensable closes #9

--- src/compra.ts
+++ src/compra.ts
@@ -1,11 +1,19 @@
 import { Producto } from "./producto";
 
 export class Compra{
-    readonly _fechaCompra: Date;
-    readonly _compra: [Producto,number][];
+    readonly fechaCompra: Date;
+    readonly compra: [Producto,number][];
 
     constructor(fecha: Date, infoProductos: [Producto,number][]){
-        this._compra = infoProductos;
-        this._fechaCompra = fecha
+        this.compra = infoProductos;
+        this.fechaCompra = fecha
+    }
+
+    getCompra(): [Producto,number][]{
+        return this.compra;
+    }
+
+    getFecha(): Date{
+        return this.fechaCompra;
     }
 }
