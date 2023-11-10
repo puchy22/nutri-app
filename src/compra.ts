f13fec3f4809373887dcc9a6e321c5415d61b01f
puchy22 <rubendltv22@gmail.com>
2023-11-10 13:10:47 +0100

    Añadida lógica de crear compra de ticket close #21 se ha añadido la lógica de negocio de la cual dado un ticket de la compra se extrae la fecha, los nombres de los productos y la cantidad de cada producto

--- src/compra.ts
+++ src/compra.ts
@@ -1,19 +1,72 @@
 import { Producto } from "./producto";
+import { Ticket } from "./ticket";
 
 export class Compra{
-    readonly fechaCompra: Date;
-    readonly compra: [Producto,number][];
+   private fechaCompra: Date;
+   private compra: [Producto,number][];
 
-    constructor(fecha: Date, infoProductos: [Producto,number][]){
-        this.compra = infoProductos;
-        this.fechaCompra = fecha
-    }
+   constructor(ticket: Ticket){
+      
+      // Extraer toda la información del ticket
+      let informacionTicket: string|null = ticket.getContenido();
 
-    getCompra(): [Producto,number][]{
-        return this.compra;
-    }
+      if (informacionTicket){
 
-    getFecha(): Date{
-        return this.fechaCompra;
-    }
+         // Extraer la fecha de compra
+      
+         const fechaCompra: RegExpMatchArray|null = informacionTicket.match(/\s*(\d{2})\/(\d{2})\/(\d{4})\s*/);
+
+         if (fechaCompra){
+
+            const [_, dia, mes, anio] = fechaCompra;
+            this.fechaCompra = new Date(parseInt(anio), parseInt(mes) - 1, parseInt(dia));
+         }
+         else{
+            this.fechaCompra = new Date();
+         }
+
+         // Extraer los productos de la compra
+         
+         this.compra = [];
+
+         // Separar todo el texto en las distintas líneas
+
+         const lineas: string[] = informacionTicket.split("\n");
+
+         // Buscar la palabra "Factura Simplificada" y "Descripción" para saber a partir de qué línea se encuentran las cantidades y le nombre de los productos
+
+         const indiceFactura: number = lineas.findIndex(linea => linea.includes("FACTURA SIMPLIFICADA"));
+
+         const indiceDescripcion: number = lineas.findIndex(linea => linea.includes("Descripción"));
+
+         for (let indiceNombreProducto = indiceDescripcion + 1, indiceCantidadProducto = indiceFactura + 2;
+               indiceNombreProducto < lineas.length && lineas[indiceNombreProducto] != "";
+               indiceNombreProducto++, indiceCantidadProducto++){
+         
+            // Extraer el nombre del producto (línea completa) y meterlo en el array de los nombres de los productos
+
+            const nombreProducto: string = lineas[indiceNombreProducto];
+
+            const cantidad: number = parseInt(lineas[indiceCantidadProducto]);
+
+            this.compra.push([new Producto(nombreProducto, 0, 0, 0), cantidad]);
+
+         }
+
+      }
+      else{
+         this.fechaCompra = new Date();
+         this.compra = [];
+      }
+
+   }
+
+   getCompra(): [Producto,number][]{
+      return this.compra;
+   }
+
+   getFecha(): Date{
+      return this.fechaCompra;
+   }
+
 }
